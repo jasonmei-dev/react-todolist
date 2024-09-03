376e0d667085e4b0a2a1e1ada001c95d5769159d
jmei403 <jason.mei403@gmail.com>
2024-09-03 09:18:36 -0400

    persist todos in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
@@ -6,14 +6,20 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [todoValue, setTodoValue] = useState('');
 
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList }));
+  }
+
   function handleAddTodo(newTodo) {
     const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
     setTodos(newTodoList);
   }
 
   function handleDeleteTodo(index) {
-    const updatedTodoList = todos.filter((todo, todoIndex) => todoIndex !== index);
-    setTodos(updatedTodoList);
+    const newTodoList = todos.filter((todo, todoIndex) => todoIndex !== index);
+    persistData(newTodoList);
+    setTodos(newTodoList);
   }
 
   function handleEditTodo(index) {
@@ -21,6 +27,20 @@ function App() {
     setTodoValue(valueToBeEdited);
     handleDeleteTodo(index);
   }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+
+    let localTodos = localStorage.getItem('todos');
+    if (!localTodos) {
+      return;
+    }
+
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, []);
 
   return (
     <>
